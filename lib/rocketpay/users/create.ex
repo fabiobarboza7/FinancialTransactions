9641d01c0488b5bd3e15b697c40907284713bf30
Fábio Souza <fabiobarboza7@hotmail.com>
2021-02-28 15:40:46 -0300

    deposit flow

--- lib/rocketpay/users/create.ex
+++ lib/rocketpay/users/create.ex
@@ -1,5 +1,5 @@
 defmodule Rocketpay.Users.Create do
-  alias Rocketpay.{User, Account}
+  alias Rocketpay.{User, Repo,  Account}
   alias Ecto.Multi
 
   def call(params) do
@@ -8,6 +8,10 @@ defmodule Rocketpay.Users.Create do
       |> Multi.run(:create_account, fn repo, %{create_user: user} ->
         insert_account(repo, user)
       end)
+      |> Multi.run(:preload_data, fn repo, %{create_user: user} ->
+        preload_data(repo, user)
+      end)
+      |> run_transaction()
   end
 
   defp insert_account(repo, user) do
@@ -20,6 +24,16 @@ defmodule Rocketpay.Users.Create do
     params = %{user_id: user_id, balance: "0.00"}
 
     Account.changeset(params)
+  end
 
+  defp preload_data(repo, user) do
+    {:ok, repo.preload(user, :account)}
+  end
+
+  defp run_transaction(multi) do
+    case Repo.transaction(multi) do
+      {:error, _operation, reason, _changes} -> {:error, reason}
+      {:ok, %{preload_data: user}} -> {:ok, user}
+    end
   end
 end
